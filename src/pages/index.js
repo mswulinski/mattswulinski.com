7e6d56cd17e2b906eb7d058409dd2068c81d7ba2
Matt <seedguardian@gmail.com>
2018-05-25 02:36:52 -0400

    TH best

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi humans</h1>
     <p>Welcome to your new Swulinski site.</p>
-    <p>Now go build something great.</p>
+    <p>Matt Swulinski is the best</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
