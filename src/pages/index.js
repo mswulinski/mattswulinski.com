95f076e2f5a8ea70ca5e81a368131d8f9c19f77d
Matt <seedguardian@gmail.com>
2018-05-25 13:15:06 -0400

    Tweaks on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi humans</h1>
+    <h1>Hi humans!</h1>
     <p>Welcome to your new Swulinski site.</p>
     <p>Matt Swulinski is the best</p>
     <Link to="/page-2/">Go to page 2</Link>
